be3bebf072da9216b5d25a49457475fe80bc49fd
edmundo papi <edmundopierre@yahoo.com>
2020-10-19 08:58:50 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,7 +4,7 @@ class SchoolClassesController < ApplicationController
     end
 
     def show
-      @school_class = SchoolClass.find_by(params[:id])
+      @school_class = SchoolClass.find(params[:id])
     end
 
     def new
@@ -17,7 +17,7 @@ class SchoolClassesController < ApplicationController
     end
 
     def edit
-      @school_class = SchoolClass.find(params[:id])
+      @school_class = SchoolClass.find_by(params[:id])
     end
 
     def update
