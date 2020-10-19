a3e34ed0b276556262e6aab3a1e94386f7fbe8a5
edmundo papi <edmundopierre@yahoo.com>
2020-10-19 07:39:20 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -2,24 +2,26 @@ class SchoolClassesController < ApplicationController
 
 def index
 end
+def show
+@school_classe = SchoolClass.find(params[:id])
+end
+
+def new
+@school_class = SchoolClass.new
+end
+
 def create
 @school_class = SchoolClass.create(school_classes_params)
 redirect_to school_class_path(@school_class)
 end
 
 
-def new
-@school_class = SchoolClass.new
-end
-
 
 def edit
 @school_classe = SchoolClass.find(params[:id])
 end
 
-def show
-@school_classe = SchoolClass.find(params[:id])
-end
+
 def update
 @school_class = SchoolClass.find(params[:id])
 @school_class.update(school_class_params)
