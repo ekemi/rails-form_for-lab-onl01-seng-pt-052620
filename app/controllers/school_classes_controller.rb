a6f448ed2050ed78e86fe201f27f64345e203ea7
edmundo papi <edmundopierre@yahoo.com>
2020-10-19 07:32:32 +0000

    Automatically backed up by Learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,32 @@
+class SchoolClassesController < ApplicationController
+
+def create
+@school_class = SchoolClass.create(school_classes_params)
+redirect_to school_class_path(@school_class)
+end
+
+
+def new
+@school_class = SchoolClass.new
+end
+
+
+def edit
+@school_classe = SchoolClass.find(params[:id])
+end
+
+def show
+@school_classe = SchoolClass.find(params[:id])
+end
+def update
+@school_class = SchoolClass.find(params[:id])
+@school_class.update(school_class_params)
+redirect_to school_class_path(@school_class)
+end
+
+private
+def school_classes_params
+  params.require(:schoolclass).permit(:title,:room_number)
+end
+
+end
